0f0ddbc2c39aa688f66277d4b0e976f50e6952a6
Adilsha007 <adilsha1234567@gmail.com>
2022-12-19 15:51:29 +0530

    second commit

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.use(function (req, res, next) {
   res.status(404).send("<h1>Sorry can't find that.... 404!</h1>");
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
   console.log('server is listening from the port .....!');
 });
